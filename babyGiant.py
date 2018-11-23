634c1aaad893f6ce92cc219cd8ae28a2011e0a2e
Nicolas Donnaint <ashcatran@gmail.com>
2018-11-23 16:16:42 +0100

    working algorithm, still needs refining

--- babyGiant.py
+++ babyGiant.py
@@ -2,9 +2,11 @@
 #!/usr/bin/env python3
 
 import math
+import time
 
 # algo from http://defeo.lu/in420/Pas%20de%20b%C3%A9b%C3%A9%20et%20pas%20de%20g%C3%A9ant
 
+#returns x**e mod n
 def modPow(x, e, n):
     result = 1
     while e > 0:
@@ -15,20 +17,38 @@ def modPow(x, e, n):
     return result
 
 if __name__ == '__main__':
-    p = 97 #prime number, defines group Z/nZ
-    g = 17
-    a = 3 # we are looking for 0<=x < p-1 such as a = g**x mod p
-    m = math.ceil(math.sqrt(p))
+    p = 101 #prime number, defines group Z/nZ
+    g = 2
+    a = 48 # we are looking for 0<=x < p-1 such as a = g**x mod p i.e. 57=3**100 mod 113
+    m = int(math.ceil(math.sqrt(p)))
+    print('m', m)
     babySteps = {} # dicts in python have a search complexity of O(1)
 
+   # print(modPow(-2,-12345,1073741827))
     for i in range(0, m):
-        babySteps[i] = modPow(g, i, p)
+       # babySteps[i] = modPow(g, i, p)
+       babySteps[modPow(g, i, p)] = i
     i = 0
 
+    print("===Baby Steps===")
     for num in babySteps:
         print(num, babySteps[num])
+    
+    r = modPow(g, -m+p-1, p) # g**(-m+p-1)
+    print("===Giant Steps===")
+    found = False
+    while found == False:
+        d = a * modPow(r, i, p) % p
+       # found = d in babySteps.itervalues()
+        found = babySteps.has_key(d)
+       # print(i, d)
+        i += 1
+       # time.sleep(1)
+    i= i-1
+    j=babySteps.get(d)
+    print(i, j)
+    res = i*m +j
+    print(res)
 
-    #while babySteps.get(modPow(a, i*m, p), True):
-    #    pass
-    print(i)
+
 
